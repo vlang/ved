200b1592a791d1cbc33ae2b19c7057fb140e3564
Alexander Medvednikov <alexander@vlang.io>
2020-11-19 15:59:01 +0100

    uiold.m and v.mod

---
+++ uiold/uiold.m
@@ -0,0 +1,20 @@
+void focus_app(void* next, void* event, void* data) {
+	NSLog(@"2The hot key was pressed.");
+	NSApplication *myApp = [NSApplication sharedApplication];
+	[myApp activateIgnoringOtherApps:YES];
+	//return noErr;
+}
+
+void reg_key_vid2() {
+	puts("REGISTERING VED KEY2");
+	EventHotKeyRef gMyHotKeyRef;
+	EventHotKeyID gMyHotKeyID;
+	EventTypeSpec eventType;
+	eventType.eventClass = kEventClassKeyboard;
+	eventType.eventKind = kEventHotKeyPressed;
+	InstallApplicationEventHandler(&focus_app, 1, &eventType, NULL, NULL);
+	gMyHotKeyID.signature = 'rml1';
+	gMyHotKeyID.id = 1;
+	RegisterEventHotKey(kVK_ANSI_1, cmdKey, gMyHotKeyID,
+		GetApplicationEventTarget(), 0, &gMyHotKeyRef);
+}
---
+++ v.mod
@@ -0,0 +1,10 @@
+Module {
+        name: 'uiold'
+        author: 'medvednikov'
+        version: '0.0.4'
+        repo_url: 'https://github.com/vlang/ui'
+        vcs: 'git'
+        tags: ['']
+        description: ''
+        license: 'MIT'
+}
