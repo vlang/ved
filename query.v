f59a34b1314f652ae2dfd1ac3c57fae1dab27886
StunxFS <56417208+StunxFS@users.noreply.github.com>
2022-05-19 19:12:02 -0400

    fix

--- query.v
+++ query.v
@@ -80,7 +80,7 @@ fn (mut ved Ved) key_query(key gg.KeyCode, super bool) {
 						line := ved.gg_lines[ved.gg_pos]
 						path := line.all_before(':')
 						pos := line.index(':') or { 0 }
-						pos2 := line.index_after(':', pos + 1)
+						pos2 := line.index_after_int(':', pos + 1)
 						// line_nr := line[path.len + 1..].int() - 1
 						line_nr := line[pos + 1..pos2].int() - 1
 						ved.view.open_file(ved.workspace + '/' + path)
@@ -352,7 +352,7 @@ fn (mut ved Ved) draw_git_grep(x int, y int) {
 		}
 		pos := line.index(':') or { continue }
 		path := line[..pos].limit(40)
-		pos2 := line.index_after(':', pos + 1)
+		pos2 := line.index_after_int(':', pos + 1)
 		if pos2 == -1 || pos2 >= line.len - 1 {
 			continue
 		}
