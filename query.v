d4046094f5465cd7541c7f202bba7aabb5503a75
Alexander Medvednikov <alexander@vlang.io>
2022-12-09 07:50:25 +0300

    minor grep fix

--- query.v
+++ query.v
@@ -446,7 +446,8 @@ fn (mut ved Ved) ctrlj_open() {
 fn (mut ved Ved) git_grep() {
 	ved.gg_pos = 0 // select the first result for faster switching to the right file =
 	// (especially if there's only one result)
-	s := os.execute('git -C "$ved.workspace" grep -F -n "$ved.search_query"')
+	query := ved.search_query.replace('$', '\\\$')
+	s := os.execute('git -C "$ved.workspace" grep -F -n "$query"')
 	if s.exit_code == -1 {
 		return
 	}
