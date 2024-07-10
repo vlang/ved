0443df98f19e1c0217f9884a98db82d8148ffaef
Alexander Medvednikov <alexander@vlang.io>
2024-07-10 14:43:51 +0300

    fix compilation on latest macos

--- uiold/uiold.m
+++ uiold/uiold.m
@@ -1,9 +1,14 @@
-void focus_app(void* next, void* event, void* data) {
+//int focus_app(void* next, void* event, void* data) {
+int focus_app(struct OpaqueEventHandlerCallRef* next, struct OpaqueEventRef * event, void* data) {
 	NSLog(@"2The hot key was pressed.");
 	NSApplication *myApp = [NSApplication sharedApplication];
 	[myApp activateIgnoringOtherApps:YES];
+	return 0;
 	//return noErr;
 }
+
+//int MyEventHandler(struct OpaqueEventHandlerCallRef *inHandlerCallRef,
+//struct OpaqueEventRef *inEvent, void *inUserData);
 
 void reg_key_ved2() {
 	puts("REGISTERING VED KEY2");
